ec62d2e3405d54edd4d496dbc9c6f61ce79b6cb2
Ruslan <upacyxou@tutanota.com>
2022-09-08 21:49:57 +0400

    remove unsued style

--- src/icons/Loading.tsx
+++ src/icons/Loading.tsx
@@ -18,7 +18,6 @@ const icon = (small?: boolean, screenCentre?: boolean) =>
     inset({ 'top-1/2': screenCentre, 'left-1/2': screenCentre }),
     position({ absolute: screenCentre })
   )
-const iconPath = opacity('opacity-100')
 
 interface LoadingProps {
   screenCentre?: boolean
@@ -29,7 +28,6 @@ export default function ({ small, screenCentre }: LoadingProps) {
   return (
     <svg className={icon(small, screenCentre)} viewBox="0 0 24 24">
       <path
-        className={iconPath}
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
